73723594d745577799bd20cc73a814295f748dc4
Abasp <abaspoor@hotmail.com>
2023-08-21 14:31:12 +0330

    lang

--- Frontend/src/components/Market/marketitem.js
+++ Frontend/src/components/Market/marketitem.js
@@ -12,11 +12,9 @@ const Marketitem = ({title,description,img,swap}) => {
         <>
         {!isMatch ?
                 <>
-		<Grid>
-        <Box padding={10} justifyContent={'space-between'} alignItems={'right'} display={'flex'} bgcolor={!swap&&'#b2f7f7'}>
-            {swap ?  <>
-                <Grid display={'flex'} flexDirection={'row'} justifyContent={'flex-end'} dir={'ltr'}>
-                    <Grid sm={12} md={8} marginRight={10} minWidth={'50%'}>
+        <Box padding={10} justifyContent={'space-between'} alignItems={'center'} display={'flex'} bgcolor={!swap&&'#b2f7f7'}>
+                <Grid display={'flex'} flexDirection={'row'} justifyContent={'flex-end'} dir={swap ? 'ltr' : 'rtl'} maxHeight={'inherit'}>
+                    <Grid sm={12} md={8} minWidth={'50%'}>
                         <Typography color={'$734950'} padding={0} variant={'h3'}>
                             {title}
                         </Typography>
@@ -24,28 +22,10 @@ const Marketitem = ({title,description,img,swap}) => {
                         <Button variant={'outlined'}>اضافه به سبد خرید</Button>
                     </Grid>
                     <Grid sm={12} xl={4}>
-                    <img src={img} alt={title} loading={'lazy'} width={'100%'} style={{boxShadow:'10px 10px 20px #b2f7f7', borderRadius:20}} height={'auto'}/>
+                    <img src={img} alt={title} loading={'lazy'} width={'100%'} height={'auto'} style={{boxShadow:'10px 10px 20px #b2f7f7', borderRadius:20}}/>
                     </Grid>
                 </Grid>
-                    </>
-                    :
-    <>
-        <Grid display={'flex'} flexDirection={'row'} justifyContent={'flex-end'} dir={'rtl'}>
-            <Grid sm={12} md={8} marginLeft={10} minWidth={'50%'}>
-                <Typography color={'$734950'} padding={0} variant={'h3'}>
-                    {title}
-                </Typography>
-                <Typography variant={'caption'} padding={0}>{description}</Typography>
-                <Button variant={'outlined'}>اضافه به سبد خرید</Button>
-            </Grid>
-            <Grid sm={12} xl={4}>
-                <img src={img} alt={title} loading={'lazy'} width={'100%'} style={{boxShadow:'10px 10px 20px #b2f7f7', borderRadius:20}} height={'auto'}/>
-            </Grid>
-        </Grid>
-            </>
-   }
         </Box>
-        </Grid>
                 </>
         :
         <h1>hello</h1>
